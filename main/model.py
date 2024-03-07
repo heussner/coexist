36068281f5841ce121120c3b380fed7145138f91
heussner <60997181+heussner@users.noreply.github.com>
2024-03-07 14:14:08 -0800

    Update model.py

--- main/model.py
+++ main/model.py
@@ -41,20 +41,30 @@ class Match:
     self.im2_cells = None
     self.arr1_shared = None
     self.arr2_shared = None
-    self.matching = None
+    self.arr1_matched = None
+    self.arr2_matched = None
 
-    
     self.im1_cellIDs = list(self.arr1[self.cellID_label])
     self.im2_cellIDs = list(self.arr2[self.cellID_label])
-    arr1_shared = self.arr1[self.shared_markers]
-    arr2_shared = self.arr2[self.shared_markers]
-    self.arr1_shared = np.array(zscore(arr1_shared, axis=0))
-    self.arr2_shared = np.array(zscore(arr2_shared, axis=0))
-    self.pairwise_distances = 1 - spearmanr(self.arr1_shared, self.arr2_shared)[0][]
+    
+    if self.shared_markers:
+      arr1_shared = self.arr1[self.shared_markers]
+      arr2_shared = self.arr2[self.shared_markers]
+      self.arr1_shared = np.array(zscore(arr1_shared, axis=0))
+      self.arr2_shared = np.array(zscore(arr2_shared, axis=0))
+      self.pairwise_distances = 1 - spearmanr(self.arr1_shared, self.arr2_shared)[0][]
 
+    def graph(self):
+      print('Making kNN graphs')
+      self.arr1_graph = None
+      self.arr2_graph = None
+    
     def match(self):
       print('Matching cells...')
-      match_table = utils.match(self.im1_mask, self.im2_mask, self.pairwise_distances)
+      if self.method == 'correlation':
+        match_table = utils.match_cells(self.im1_mask, self.im2_mask, self.pairwise_distances)
+      elif self.method == 'overlap':
+        match_table = utils.track_cells(self.im1_mask, self.im2_mask)
       print('Removing low quality matches...')
       match_table = match_table[match_table['score']<0]
       print(f'Matched {len(match_table)} cells of {len(self.arr1)} slide 1 cells and {len(self.arr2)} slide 2 cells')
@@ -63,5 +73,10 @@ class Match:
       return None
 
     def cluster(self):
-      
+      print('Clustering matched cells...')
+      self.matched_labels = None
+      print('Propagating labels to unmatched cells...')
+      self.arr1_labels = None
+      self.arr2_labels = None
+      return None
       
