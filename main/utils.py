03b6425df5e62fa8f7298ec353e6cc06b1a9d14e
heussner <60997181+heussner@users.noreply.github.com>
2024-03-07 14:01:10 -0800

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -81,3 +81,88 @@ def match_cells(im1_mask, im2_mask, pairwise_distances):
     match_table = pd.DataFrame(data={'im1_cell':cellID,'im2_cell':list(im2_frame['label']),'score':scores})
     match_table = match_table[match_table['im1_cell']!=0]
     return match_table
+
+def track_cells(im1_mask, im2_mask):
+    """
+    Description:
+    -Track target cells to reference cells
+    
+    Parameters:
+    -im1_mask: uint array, Cell mask for target image
+    -im2_mask: uint array, Cell mask for reference image
+    
+    Return:
+    -match_table: list, target cell IDs tracked to the ordered list of reference cell IDs
+    """
+    props = ['label','coords','centroid','mean_intensity']
+    vote_threshold=overlap
+    im1_frame = pd.DataFrame(regionprops_table(im1_mask, im1_mask, properties=props)) # target regionprops
+    im2_frame = pd.DataFrame(regionprops_table(im2_mask, im2_mask, properties=props)) # reference regionprops
+
+    cellID = np.zeros(len(im2_frame)) # cell ID map from reference to target
+
+    for i in range(len(im2_frame)): # for each region in reference
+
+        coords = im2_frame['coords'][i].squeeze()
+        x = im1_mask[tuple(coords.T)] # get target where reference region is
+
+        ux, n = np.unique(x, return_counts=True) # get counts of unique pixel values of target in reference region
+        area = np.sum(n)
+
+        if ux[0] == 0: # remove background pixels from unique list
+            ux = ux[1:]
+            n = n[1:]
+
+        if (len(n) != 0) and (max(n)/area > vote_threshold):#if the largest cell in target is above pixel threshold
+
+            if len(n[n==max(n)]) > 1:# case where multiple equally-sized target cells in reference region
+                id_ = ux[n==max(n)] # cellID list
+                dist1 = np.zeros(id_.shape)
+                for j in range(len(id_)): # iterate cells
+                    xdist = im2_frame["centroid-0"][i]- im1_frame["centroid-0"][im1_frame['label']==id_[j]].squeeze()
+                    ydist = im2_frame["centroid-1"][i]- im1_frame["centroid-1"][im1_frame['label']==id_[j]].squeeze()
+                    dist1[j] = (xdist**2 + ydist**2)**0.5
+                    
+                cellID[i] = id_[np.where(dist1==min(dist1))]
+
+                # case where cells are equally close to centroid
+
+            else:
+                cellID[i] = ux[np.where(n==max(n))]
+
+        else:
+            cellID[i] = 0
+
+        #checking cyclic consistency for uniqueness
+        if cellID[i] != 0:
+            idx = im1_frame['coords'][im1_frame['label'] == cellID[i]].squeeze() # get target coords in reference cell id
+            y = im2_mask[tuple(idx.T)].copy() # get reference array where target cell is
+            uy, m = np.unique(y, return_counts=True)
+            
+            if uy[0] == 0: # remove background pixels from unique list
+                uy = uy[1:]
+                m = m[1:]
+            
+            if (len(m[m==max(m)]) > 1):
+                id_ = uy[m==max(m)]
+                dist2 = np.ones(id_.shape)*100000
+                for j in range(len(id_)):
+                    x_dist = im1_frame["centroid-0"][im1_frame['label']==cellID[i]].squeeze() - im2_frame["centroid-0"][im2_frame['label']==id_[j]].squeeze()
+                    y_dist = im1_frame["centroid-1"][im1_frame['label']==cellID[i]].squeeze() - im2_frame["centroid-1"][im2_frame['label']==id_[j]].squeeze()
+                    dist2[j] = (x_dist**2 + y_dist**2)**0.5
+
+                if uy[np.where(dist2==min(dist2))] != im2_frame["label"][i]:
+                    cellID[i] = 0
+
+            elif (len(m[m == max(m)]) == 1) and (uy[m==max(m)] != im2_frame["label"][i]):
+                cellID[i] = 0
+    match_table = pd.DataFrame(data={'im1_cell':cellID,'im2_cell':list(im2_frame['label'])})
+    match_table = match_table[match_table['im1_cell']!=0]
+    return match_table
+
+def ids_to_table(ids, table):
+    new_table = table[table['CellID'].isin(ids)] # get relevant rows
+    df1 = new_table.set_index('CellID')
+    new_table = df1.reindex(ids) # set new table in correct order
+    new_table.reset_index(inplace=True)
+    return new_table
